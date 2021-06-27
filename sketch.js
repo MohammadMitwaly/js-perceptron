94aa19d402c4343b58d5f48ccb660b249299a78b
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-06-27 09:28:06 +0300

    Add the aility to train automatically

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const perceptron = new Perceptron();
 const inputs = fillArray(2, -1, 1);
 let points;
+let trainingIndex = 0;
 
 function setup() {
   createCanvas(750, 750);
@@ -24,13 +25,17 @@ function draw() {
     noStroke();
     ellipse(point.x, point.y, 11.25, 11.25);
   });
+
+  trainSpecificIndex();
+  // If we have already trained all of the points, loop back to train again from the start,
+  // else, continue to train the next point, until we are back at the start
+  trainingIndex === points.length - 1 ? (trainingIndex = 0) : trainingIndex++;
 }
 
-// Training the perceptron on each mouse click
-function mousePressed() {
-  points.forEach((point) => {
-    const inputs = [point.x, point.y];
-    const targetLabel = point.label;
-    perceptron.train(inputs, targetLabel);
-  });
+function trainSpecificIndex() {
+  console.log(trainingIndex);
+  const point = points[trainingIndex];
+  const inputs = [point.x, point.y];
+  const targetLabel = point.label;
+  perceptron.train(inputs, targetLabel);
 }
