4fd74c2968bcb25ded7a19ffe792a370e878908a
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-07-14 19:25:32 +0300

    Add initial matrix library implementation, will be swapped for a custom library

---
+++ MatrixMathLib/matrix.js
@@ -0,0 +1,20 @@
+class Matrix {
+  constructor(amountOfRows, amountOfColumns, defaultValue) {
+    this.rows = amountOfRows;
+    this.cols = amountOfColumns;
+    this.defaultValue = defaultValue !== undefined ? defaultValue : 0;
+    this.values = this.initMatrix(this.rows, this.cols, this.defaultValue);
+  }
+
+  // Source: https://stackoverflow.com/a/13808461/6236395
+  initMatrix(w, h, val) {
+    var arr = [];
+    for (let i = 0; i < h; i++) {
+      arr[i] = [];
+      for (let j = 0; j < w; j++) {
+        arr[i][j] = val;
+      }
+    }
+    return arr;
+  }
+}
