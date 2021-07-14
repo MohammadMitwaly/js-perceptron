dbcaf64a4a1a4dbca07c601c186768c394d509ef
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-07-14 19:35:22 +0300

    Add basic test

--- Multi-Layer Perceptron/sketch.js
+++ Multi-Layer Perceptron/sketch.js
@@ -1,5 +1,10 @@
+const matrix = new Matrix(3, 3, 2);
+
 function setup() {
   createCanvas(400, 400);
+  console.log(matrix);
+  matrix.addScalar(2);
+  console.log(matrix.values);
 }
 
 function draw() {
