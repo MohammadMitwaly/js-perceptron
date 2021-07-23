5c564064ea55a6da93a182fe6c20da86e7577b1f
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-07-23 20:20:37 +0300

    Add initial tests

--- MatrixMathLib/matrix.test.js
+++ MatrixMathLib/matrix.test.js
@@ -1,1 +1,11 @@
 const Matrix = require("./matrix");
+
+test("Create matrix", () => {
+  const testM = new Matrix(3, 3);
+  expect(testM).toBeInstanceOf(Matrix);
+});
+
+test("Create matrix without default value", () => {
+  const testM = new Matrix(3, 3);
+  expect(testM.values[0][0]).toBe(0);
+});
