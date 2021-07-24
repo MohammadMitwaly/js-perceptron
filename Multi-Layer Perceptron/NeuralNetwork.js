3b62d3e24b1d7abb58b6072986592467b6d9d4a8
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-07-24 12:32:53 +0300

    Update NN

--- Multi-Layer Perceptron/NeuralNetwork.js
+++ Multi-Layer Perceptron/NeuralNetwork.js
@@ -3,5 +3,15 @@ class NeuralNetwork {
     this.inputNodes = inputAmount;
     this.hiddenNodes = hiddenNeurons;
     this.outputNodes = outputAmount;
+    // The weight matrix between the input nodes and the hidden layer
+    this.weightsInputsHiddenLayer = new Matrix(
+      this.hiddenNodes,
+      this.inputNodes
+    );
+    // The weight matrix for the hidden layer and the output nodes
+    this.weightsHiddenLayerOutputs = new Matrix(
+      this.outputNodes,
+      this.hiddenNodes
+    );
   }
 }
--- Multi-Layer Perceptron/sketch.js
+++ Multi-Layer Perceptron/sketch.js
@@ -1,17 +1,6 @@
-const matrix1 = new Matrix(2, 3);
-const matrix2 = new Matrix(3, 2);
-
+const neuralNetwork = new NeuralNetwork(2, 2, 1);
 function setup() {
   createCanvas(400, 400);
-  matrix1.randomizeValues(1, 5);
-  matrix2.randomizeValues(1, 5);
-
-  // Table is a helper function that calls console.table, find it in Utils/util.js
-  table(matrix1.values);
-  table(matrix2.values);
-
-  const matrix3 = Matrix.multiply(matrix1, matrix2);
-  table(matrix3.values);
 }
 
 function draw() {
